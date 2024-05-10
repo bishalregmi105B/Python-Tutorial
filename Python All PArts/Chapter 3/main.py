9a257ab44ea71d6814513ee4dbfb3dc89d8cc510
bishalregmi105B <89974678+bishalregmi105B@users.noreply.github.com>
2024-05-10 10:06:51 +0545

    Part 3

---
+++ Python All PArts/Chapter 3/main.py
@@ -0,0 +1,34 @@
+# a = 'Bishal '
+# b = f"{a}duufuf"
+# c = ''' Bishal
+# jxjxjx
+# dhdhdhdhhd
+# dhdhdh'''
+# print(b)
+
+## String Slicing
+# a = "Bishal"
+# print(a[0:2])
+
+# br = "Nepalese"
+# print(br[0:3:2])
+
+# a = "Like Subscribe"
+# b = len(a)
+# print(b)
+
+# a = "Like"
+# print(a.endswith("e"))
+
+# a= "dhdh"
+# print(a.count("d"))
+
+# a = "hello Guyys Like subscribe"
+# print(a.capitalize())
+
+# a = "Bishal Regmi"
+# a= a.replace("a","b")
+# print(a)
+# print(a.find("a"))
+
+print('Hello \n World')
