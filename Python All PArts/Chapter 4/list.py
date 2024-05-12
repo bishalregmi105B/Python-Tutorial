3d9d7bcea12b586964a3183a27247a40603f2b66
bishalregmi105B <89974678+bishalregmi105B@users.noreply.github.com>
2024-05-12 10:04:09 +0545

    Chapter 4

---
+++ Python All PArts/Chapter 4/list.py
@@ -0,0 +1,33 @@
+# a = ['list',1,0.9,True]
+fr = ['mango','apple']
+# b = a[0:3]# b = ['list', 1, 0.9]
+# print(b[2])
+
+# a = [1,3,2,4,5,6]
+# a.sort()
+# print(a)
+
+# a = [1,3,2,4,3]
+# a.reverse()
+# print(a)
+
+# a = [1,3,4,5,6]
+# a.insert(2,"a")
+# print(a)
+
+# a = [1,3,4,5]
+# a.append(8)
+# print(a)
+
+# a = [1,3,5]
+# a.pop(0)
+# print(a)
+
+# a = [1,"a","b",4,5,6]
+# a.remove("b")
+# print(a)
+
+# a= (1,3,1,1,1,1,1,1,1,1,4)
+# # print(a.count(1))
+
+# print(a.index(4))
